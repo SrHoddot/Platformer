f7ead2a0521997b59de235ce070febff03845db1
SrHoddot <86936894+SrHoddot@users.noreply.github.com>
2021-07-13 08:38:38 +0800

    Platformer.cs

---
+++ Platformer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Platformer : MonoBehaviour 
+{
+	Rigidbody2D rb;
+
+	public float jumpForce = 20;
+
+	public float moveSpeed = 5f;
+
+	public bool isGrounded;
+	public bool isJumping;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+
+	void Update()
+	{
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movement * Time.deltaTime * moveSpeed;
+
+
+		if (Input.GetKeyDown(KeyCode.W) && isGrounded == true)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+			isJumping = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.W) && isJumping == true)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+			isJumping = false;
+		}
+
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			SceneManager.LoadScene("Platformer");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		isGrounded = false;
+	}
+}
